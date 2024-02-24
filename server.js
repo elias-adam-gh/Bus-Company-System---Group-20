a0ad010fa9ed1226b4408825c9ba98684e61004a
Adam Elias <122506412+elias-adam-gh@users.noreply.github.com>
2024-02-24 12:05:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const path = require('path')
-const { logger, logEvents } = require('./middleware/logger')
-const errorHandler = require('./middleware/errorHandler')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const connectDB = require('./config/dbConn')
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 3000
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const connectDB = require('./config/connectDB');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const verifyRoles = require('./middleware/verifyRoles');
 
-console.log(process.env.NODE_ENV)
+const app = express();
+const PORT = process.env.PORT || 3500;
 
-connectDB()
+app.use(logger); 
+app.use(express.json()); 
+app.use(cookieParser()); 
+app.use(cors(corsOptions));
 
-app.use(logger)
+connectDB().then(() => console.log('Connected to MongoDB')).catch(err => console.error(`MongoDB connection error: ${err}`));
 
-app.use(cors(corsOptions))
+app.use('/', require('./routes/root')); 
+app.use('/users', [verifyRoles('admin')], require('./routes/userRoutes')); 
 
-app.use(express.json())
+app.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
 
-app.use(cookieParser())
+app.use(errorHandler);
 
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.use('/', require('./routes/root'))
-app.use('/users', require('./routes/userRoutes'))
-
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
-
-app.use(errorHandler)
-
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-})
-
-mongoose.connection.on('error', err => {
-    console.log(err)
-    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
